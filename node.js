87a23a10d72ba28859a08e9b47e32a4e91906e41
v-homyakov <v-homyakov@yandex-team.ru>
2024-06-12 17:58:38 +0400

    add node 22 fetch

---
+++ node.js
@@ -0,0 +1,13 @@
+import {readFileSync} from "node:fs";
+import pAll from "p-all";
+import {extractUrls, measure, warmup} from "./utils.js";
+
+const json = readFileSync(new URL("1500-deps.json", import.meta.url));
+const urls = extractUrls(json);
+const opts = {concurrency: process.argv[2] ? Number(process.argv[2]) : 96};
+
+const getUrl = url => fetch(url).then(res => res.text());
+await warmup(urls, getUrl);
+
+const promise = pAll(urls.map(url => () => getUrl(url)), opts);
+await measure('node', promise);
